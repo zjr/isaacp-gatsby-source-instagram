09ad08f49af517aa091f5fd3a68d52951a6815a1
pak11273 <isaac.pak.2024@gmail.com>
2022-06-02 12:39:43 -0500

    refactor: update isaacp-gatsby-source-instagram plugin

--- src/instagram.js
+++ src/instagram.js
@@ -7,10 +7,12 @@ export async function getInstagramPosts({
   instagram_id,
 }) {
   return axios.get(
-      `https://graph.facebook.com/${instagram_id}?fields=business_discovery.username(${customer_username}){media{media_url}}&access_token=${access_token}`
-    )
-    .then((response) => response.data.business_discovery.media.data)
-    .catch((err) => {
+    `https://graph.facebook.com/${instagram_id}?fields=business_discovery.username(${customer_username}){media{media_url}}&access_token=${access_token}`
+  )
+    .then(async (response) => {
+      const data = await response.data.business_discovery.media.data
+      return data
+  }).catch((err) => {
       console.warn(
         `\nCould not get instagram posts using the Graph API. Error status ${err}`
       )
