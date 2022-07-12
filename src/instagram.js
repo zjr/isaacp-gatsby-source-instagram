6081483cf4ba0797f63dc41163ae75f837615985
pak11273 <isaac.pak.2024@gmail.com>
2022-07-12 07:47:43 -0500

    fix: endpoint

--- src/instagram.js
+++ src/instagram.js
@@ -6,9 +6,12 @@ export async function getInstagramPosts({
   customer_username,
   instagram_id,
 }) {
+  // original endpoint
+  // `https://graph.facebook.com/${instagram_id}?fields=business_discovery.username(${customer_username}){media{media_url}}&access_token=${access_token}`
+
   try {
     const res = await axios.get(
-      `https://graph.facebook.com/${instagram_id}?fields=business_discovery.username(${customer_username}){media{media_url,media_type,owner,media_product_type, children{media_url,media_type}}}&access_token=${access_token}`
+      `https://graph.facebook.com/${instagram_id}?fields=business_discovery.username(${customer_username}){media{media_url, media_type, owner, media_product_type, children{media_url, media_type}}}&access_token=${access_token}`
     )
 
     return res.data.business_discovery.media.data
